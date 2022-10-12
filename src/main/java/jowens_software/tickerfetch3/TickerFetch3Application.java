e0cd5658ab1cc65d906b55695838cb44963dc522
Jared <60214343+jowens-self@users.noreply.github.com>
2022-10-12 15:01:15 -0500

    Initial commit

---
+++ src/main/java/jowens_software/tickerfetch3/TickerFetch3Application.java
@@ -0,0 +1,13 @@
+package jowens_software.tickerfetch3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickerFetch3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TickerFetch3Application.class, args);
+    }
+
+}
---
+++ src/test/java/jowens_software/tickerfetch3/TickerFetch3ApplicationTests.java
@@ -0,0 +1,13 @@
+package jowens_software.tickerfetch3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickerFetch3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
