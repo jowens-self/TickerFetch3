10ae04be4d8cc53d2fa4782e3d314c96e4cf8389
Jared <60214343+jowens-self@users.noreply.github.com>
2022-10-19 23:54:18 -0500

    Save before battery died; semi figured out issue on Quote call

--- src/main/java/jowens_software/tickerfetch3/TickerFetch3Application.java
+++ src/main/java/jowens_software/tickerfetch3/TickerFetch3Application.java
@@ -4,6 +4,7 @@ import com.crazzyghost.alphavantage.AlphaVantage;
 import com.crazzyghost.alphavantage.AlphaVantageException;
 import com.crazzyghost.alphavantage.Config;
 
+import com.crazzyghost.alphavantage.timeseries.response.QuoteResponse;
 import com.crazzyghost.alphavantage.timeseries.response.TimeSeriesResponse;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -30,25 +31,55 @@ public class TickerFetch3Application {
 
         //Not seeing why this one doesn't work and intraday does with
         //the object being returned to timeSeries
-        AlphaVantage.api().timeSeries().quote()
-                .forSymbol("SPY")
-                .onSuccess(e -> handleSuccess((TimeSeriesResponse) e))
-                .onFailure(TickerFetch3Application::handleFailure)
-                .fetch();
-
+//        AlphaVantage.api().timeSeries().quote()
+//                .forSymbol("SPY")
+//                .onSuccess(e -> handleSuccess((TimeSeriesResponse) e))
+//                .onFailure(TickerFetch3Application::handleFailure)
+//                .fetch();
+//
         AlphaVantage.api().timeSeries().intraday()
                 .forSymbol("SPY")
                 .onSuccess(e -> handleSuccess((TimeSeriesResponse) e))
                 .onFailure(TickerFetch3Application::handleFailure)
                 .fetch();
+
+        /* cannot call TimeSeriesResponse as does not derive from
+        the TimeSeries class, call quote version instead */
+        AlphaVantage.api().timeSeries().quote()
+                .forSymbol("SPY")
+                .onSuccess(e -> handleSuccess((QuoteResponse) e))
+                .onFailure(TickerFetch3Application::handleFailure)
+                .fetch();
+    }
+
+    //public static void handleSuccess(QuoteResponse response) {
+    public void handleSuccess(QuoteResponse response) {
+        Quote q = new Quote();
+        //plotGraph(response.getStockUnits());
+        System.out.println(QuoteResponse.QuoteParser.toJSON();
     }
 
     public static void handleSuccess(TimeSeriesResponse response) {
         //plotGraph(response.getStockUnits());
-        System.out.println(response.getStockUnits());
+
+        System.out.println(response.getStockUnits().);
     }
+
     public static void handleFailure(AlphaVantageException error) {
         /* uh-oh! */
     }
 
+
+    public class Quote {
+        String symbol,
+        double open,
+        double high,
+        double low,
+        double price,
+        double volume,
+        String latestTradingDay,
+        double previousClose,
+        double change,
+        double changePercent
+    }
 }
