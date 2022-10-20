e3e4bff1c0e7df1e4b6bc7cecbfcca198faa5ad5
Jared <60214343+jowens-self@users.noreply.github.com>
2022-10-19 23:59:12 -0500

    Rough Quote Object

--- src/main/java/jowens_software/tickerfetch3/TickerFetch3Application.java
+++ src/main/java/jowens_software/tickerfetch3/TickerFetch3Application.java
@@ -70,16 +70,109 @@ public class TickerFetch3Application {
     }
 
 
-    public class Quote {
-        String symbol,
-        double open,
-        double high,
-        double low,
-        double price,
-        double volume,
-        String latestTradingDay,
-        double previousClose,
-        double change,
-        double changePercent
+    public static class Quote {
+        private String symbol;
+        private double open;
+        private double high;
+        private double low;
+        private double price;
+        private double volume;
+        private String latestTradingDay;
+        private double previousClose;
+        private double change;
+        private double changePercent;
+
+        public Quote(String symbol, double open, double high, double low, double price, double volume, String latestTradingDay, double previousClose, double change, double changePercent) {
+            this.symbol = symbol;
+            this.open = open;
+            this.high = high;
+            this.low = low;
+            this.price = price;
+            this.volume = volume;
+            this.latestTradingDay = latestTradingDay;
+            this.previousClose = previousClose;
+            this.change = change;
+            this.changePercent = changePercent;
+        }
+
+        public String getSymbol() {
+            return symbol;
+        }
+
+        public void setSymbol(String symbol) {
+            this.symbol = symbol;
+        }
+
+        public double getOpen() {
+            return open;
+        }
+
+        public void setOpen(double open) {
+            this.open = open;
+        }
+
+        public double getHigh() {
+            return high;
+        }
+
+        public void setHigh(double high) {
+            this.high = high;
+        }
+
+        public double getLow() {
+            return low;
+        }
+
+        public void setLow(double low) {
+            this.low = low;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+        public double getVolume() {
+            return volume;
+        }
+
+        public void setVolume(double volume) {
+            this.volume = volume;
+        }
+
+        public String getLatestTradingDay() {
+            return latestTradingDay;
+        }
+
+        public void setLatestTradingDay(String latestTradingDay) {
+            this.latestTradingDay = latestTradingDay;
+        }
+
+        public double getPreviousClose() {
+            return previousClose;
+        }
+
+        public void setPreviousClose(double previousClose) {
+            this.previousClose = previousClose;
+        }
+
+        public double getChange() {
+            return change;
+        }
+
+        public void setChange(double change) {
+            this.change = change;
+        }
+
+        public double getChangePercent() {
+            return changePercent;
+        }
+
+        public void setChangePercent(double changePercent) {
+            this.changePercent = changePercent;
+        }
     }
 }
