032ac94182642376e4e398e5c6b4c53b3ec9266b
Jared <60214343+jowens-self@users.noreply.github.com>
2022-10-25 01:40:57 -0500

    Working but get method for ticker is a mess. Need to figure out the onSuccess Method

--- src/main/java/jowens_software/tickerfetch3/SingleQuoteController.java
+++ src/main/java/jowens_software/tickerfetch3/SingleQuoteController.java
@@ -6,7 +6,11 @@ import com.crazzyghost.alphavantage.AlphaVantageException;
 import com.crazzyghost.alphavantage.timeseries.response.QuoteResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicReference;
+
+@RestController
 public class SingleQuoteController {
 
 //    @RequestMapping(value = "/error")
@@ -14,17 +18,40 @@ public class SingleQuoteController {
 //        return "Error handling";
 //    }
 
+    @GetMapping("/")
+    public String index() {
+        return "Select ticker with URL until FE is linked.";
+    }
 
     @GetMapping("/quote/{ticker}")
-    void SingleQuote(@PathVariable String ticker) {
+    public AtomicReference<String> SingleQuote(@PathVariable String ticker) {
         System.out.println("Fetched Quote for " + ticker);
 
+        AtomicReference<String> result = new AtomicReference<>("");
         AlphaVantage.api().timeSeries().quote()
+
                 .forSymbol(ticker)
-                .onSuccess(e -> handleSuccess((QuoteResponse) e))
+                //.onSuccess(e -> handleSuccess((QuoteResponse) e))
+                .onSuccess(e -> {
+                        result.set(fetchResult((QuoteResponse) e));
+                    }
+                )
                 .onFailure(TickerFetch3Application::handleFailure)
                 .fetch();
+        return result;
     }
+
+    public String fetchResult(QuoteResponse response) {
+        SingleQuote q = new SingleQuote(response.getSymbol(), response.getOpen(),
+                response.getHigh(), response.getLow(), response.getPrice(),
+                response.getVolume(), response.getLatestTradingDay(),
+                response.getPreviousClose(), response.getChange(),
+                response.getChangePercent());
+
+        //if (q != null)
+            return q.toString();
+    }
+
 
     public void handleSuccess(QuoteResponse response) {
         SingleQuote q = new SingleQuote(response.getSymbol(), response.getOpen(),
